7bf9779e7b4d2e4fedd4b78046802c4f9f2a454e
Nikita Pekin <contact@nikitapek.in>
2018-02-08 22:31:34 -0500

    docs: add documentation for the addressing modes
    Add documentation for all of the variants of the AddressingMode enum.

--- src/instruction.rs
+++ src/instruction.rs
@@ -17,14 +17,36 @@ use opcode::Opcode;
 /// | (Indirect, X) | LDA ($02, X) | A <- M(PTR($02 + X)) |
 /// | (Indirect), Y | LDA ($02), Y | A <- M(PTR($02) + Y) |
 pub enum AddressingMode {
+    /// Operate on an 8-bit constant specified in the instruction.
     Immediate,
+    /// Address to be accessed is an 8-bit operand pointing to a memory location
+    /// in the zero page ($0000-$00FF).
     ZeroPage,
+    /// Address to be accessed is a zero page address calculated by taking the
+    /// 8-bit operand and adding the current value of the `X` register to it.
     ZeroPageX,
+    /// Address to be accessed is a zero page address calculated by taking the
+    /// 8-bit operand and adding the current value of the `Y` register to it.
     _ZeroPageY,
+    /// Address to be accessed is the 16-bit operand following the instruction.
     Absolute,
+    /// Address to be accessed is calculated by taking the 16-bit operand
+    /// following the instruction and adding the current value of register `X`
+    /// to it.
     AbsoluteX,
+    /// Address to be accessed is calculated by taking the 16-bit operand
+    /// following the instruction and adding the current value of register `Y`
+    /// to it.
     AbsoluteY,
+    /// Address to be accessed is calculated by taking the 8-bit operand
+    /// following the instruction, adding the current value of register `X` to
+    /// it, and retrieving the value at the resulting memory address in the zero
+    /// page.
     IndirectX,
+    /// Address to be accessed is calculated by taking the 8-bit operand
+    /// following the instruction, retrieving the value at the memory address in
+    /// the zero page corresponding to the operand, and adding the current value
+    /// of register `Y` to it.
     IndirectY,
 }
 
